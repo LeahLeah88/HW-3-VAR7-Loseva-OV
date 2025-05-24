3624289abf6b9daa50b590ed164d562667f82b37
Olga Loseva <160180819+LeahLeah88@users.noreply.github.com>
2025-05-24 19:25:25 +0300

    Задание №2.
    Добавлено задание №2.

--- HW-3/HW-3.cpp
+++ HW-3/HW-3.cpp
@@ -22,8 +22,33 @@ void task1()
 
     calculate_point(x, y);
 }
+void task2()
+{
+    system("chcp 1251>nul");
+
+    std::string a;
+    std::cin >> a;
+
+    if (a == "понедельник")
+        std::cout << "09:00 - 14:00";
+    else if (a == "вторник")
+        std::cout << "14:00 - 19:00";
+    else if (a == "среда")
+        std::cout << "09:00 - 14:00";
+    else if (a == "четверг")
+        std::cout << "14:00 - 19:00";
+    else if (a == "пятница")
+        std::cout << "09:00 - 14:00";
+    else if (a == "суббота")
+        std::cout << "Выходной";
+    else if (a == "воскресенье")
+        std::cout << "Выходной";
+    else
+        std::cout << "Некорректный день недели";
+}
 
 int main()
 {
     task1();
+    task2();
 }
