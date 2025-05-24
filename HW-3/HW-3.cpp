d45c7a69f55c753fca60e0e8defa0ae54d2c02d5
Olga Loseva <160180819+LeahLeah88@users.noreply.github.com>
2025-05-24 16:55:43 +0300

    Add project files.
    Add project files.

---
+++ HW-3/HW-3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+
+void calculate_point(int x, int y)
+{
+    if (y > abs(x) && sqrt(pow(x, 2) + pow(y, 2)) < 15)
+        std::cout << "Да";
+    else if (y == abs(x) && sqrt(pow(x, 2) + pow(y, 2)) == 15)
+        std::cout << "На границе";
+    else
+        std::cout << "Нет";
+}
+void task1()
+{
+    setlocale(LC_ALL, "Russian");
+    int x;
+    int y;
+    std::cout << "x = ";
+    std::cin >> x;
+    std::cout << "y = ";
+    std::cin >> y;
+
+    calculate_point(x, y);
+}
+
+int main()
+{
+    task1();
+}
